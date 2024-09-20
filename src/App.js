4fadba2ee4cc1adab2c6a35acb3c74fe287f9234
Mayko Costa <maykopr@hotmail.com>
2024-09-20 15:37:56 -0300

    style: Adicionando o prettier

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React, { useState } from 'react'
-import './bootstrap.min.css'
-import './App.css'
-import Tabela from './components/tabela'
-import Altura from './components/altura'
-import Peso from './components/peso'
-import Calcular from './components/calcular'
-import Resultado from './components/resultado'
+import React, { useState } from "react";
+import "./bootstrap.min.css";
+import "./App.css";
+import Tabela from "./components/tabela";
+import Altura from "./components/altura";
+import Peso from "./components/peso";
+import Calcular from "./components/calcular";
+import Resultado from "./components/resultado";
 
 export default function App() {
-  const [peso, setPeso] = useState(0);
-  const [altura, setAltura] = useState(0);
-  const [resultado, setResultado] = useState(0);
+      const [peso, setPeso] = useState(0);
+      const [altura, setAltura] = useState(0);
+      const [resultado, setResultado] = useState(0);
 
-  return (
-    <main class="calculadora d-flex flex-column">
-      <Peso p={peso} setP={setPeso} />
-      <Altura a={altura} setA={setAltura} />
-      <Calcular a={altura} p={peso} setR={setResultado} />
-      <Resultado r={resultado} />
-      <Tabela />
-    </main>
-  )
+      return (
+            <main class="calculadora d-flex flex-column">
+                  <Peso p={peso} setP={setPeso} />
+                  <Altura a={altura} setA={setAltura} />
+                  <Calcular a={altura} p={peso} setR={setResultado} />
+                  <Resultado r={resultado} />
+                  <Tabela />
+            </main>
+      );
 }
