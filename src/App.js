568d9008f0138bb523c606baaa02cf595dfc13a9
Mayko Costa <maykopr@hotmail.com>
2024-10-01 10:56:37 -0300

    refactor: alterando os nomes dos componentes e removendo importações desnecessárias

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import "./bootstrap.min.css";
 import "./App.css";
 import Tabela from "./components/tabela";
-import Altura from "./components/altura";
-import Peso from "./components/peso";
+import AlturaInput from "./components/AlturaInput";
+import PesoInput from "./components/PesoInput";
 import CalcularBtn from "./components/Calcular-btn";
-import Resultado from "./components/resultado";
+import Resultado from "./components/ResultadoInput";
 
 export default function App() {
       const [peso, setPeso] = useState(0);
@@ -18,10 +18,10 @@ export default function App() {
 
       return (
             <main className="calculadora d-flex flex-column">
-                  <Peso p={peso} setP={setPeso} />
-                  <Altura a={altura} setA={setAltura} />
+                  <PesoInput p={peso} setP={setPeso} />
+                  <AlturaInput altura={altura} setAltura={setAltura} />
                   <CalcularBtn func={calc} />
-                  <Resultado r={resultado} />
+                  <Resultado resultado={resultado} />
                   <Tabela />
             </main>
       );
--- src/components/resultado.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Resultado(props) {
-    return (
-        <div>
-            <p class="display-6 text-center">Resultado:{" " + props.r.toFixed(2)}</p>
-        </div>
-    )
-}
